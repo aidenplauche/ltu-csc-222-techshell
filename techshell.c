4739b139d3c57fd27fb0bbc999a8d1a3ff33676f
Aiden Plauche <aiden.plauche@gmail.com>
2023-02-11 17:53:32 -0600

    Techshell Command Parsing
    - User input
    - Command parsing
    - Execution
    - cd, exit builtins
    - Basic error handling
    - Gets i/o redirection filenames
    
    Co-Authored-By: Cullen <101290817+Cduval911@users.noreply.github.com>

--- techshell.c
+++ techshell.c
@@ -1,40 +1,105 @@
 //* Techshell - Aiden Plauche, Cullen Duval - CSC 222 001 - Winter 2022-2023 *//
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <wait.h>
+#define DEBUG 1
 
 // Displays cwd and returns user input
 char* CommandPrompt() {
-    // "pwd$ ", scanf
+    char* input = malloc(sizeof(char) * 256);
+    char cwd[256];
+    getcwd(cwd, sizeof(cwd));
+    printf("%s$ ", cwd);
+    
+    fgets(input, 256, stdin);
+    input[strlen(input) - 1] = '\0'; // replace newline with null
+
+    if (strcmp(input, "exit") == 0) exit(0);
+
+    return input;
 }
 
 // Wrapper for shell command
 struct ShellCommand {
     char* binary; // the binary to execute (e.g., ls);
-    char** arguments; // the arguments to send to the binary;
-    // additional fields for i/o redirection
+    char* arguments[256]; // the arguments to send to the binary;
+    char* input_file; // file to redirect stdin to
+    char* output_file; // file to redirect stdout to
+    int length; // number of arguments
 };
 
 // Processes the user input as a Shell Command
 struct ShellCommand ParseCommandLine(char* input) {
     // string parsing
     // check for i/o redirection first (< in, > out)
+    struct ShellCommand command = {NULL, {""}, NULL, NULL, 0}; // initializes shellCommand structure
+
+    // stores the arguments from input into the structure variable arguments
+    command.arguments[command.length] = strtok(input, " "); // split input by spaces
+    command.binary = command.arguments[command.length]; // sets binary to first argument
+    char* arg = strtok(NULL, " ");
+    do {
+        if (strcmp(arg, "<") == 0) {
+            command.input_file = strtok(NULL, " ");
+            arg = strtok(NULL, " ");
+            if (!arg) break;
+            else if (strcmp(arg, ">") != 0) break;
+        } 
+        if (strcmp(arg, ">") == 0) {
+            command.output_file = strtok(NULL, " ");
+            break;
+        }
+        
+        command.arguments[++command.length] = arg; // get next piece
+        arg = strtok(NULL, " ");
+    } while (arg != NULL); // token is null after last piece
+
+    // Debug
+    if (DEBUG) {
+        printf("bin %s\n", command.binary);
+        for(int i = 0; i<=command.length ;i++){
+            printf("arg: %s\n",command.arguments[i]);
+        }
+        printf("inputfile %s\n", command.input_file ? command.input_file : "NULL");
+        printf("outputfile %s\n", command.output_file ? command.output_file : "NULL");
+        printf("length %i\n", command.length);
+    }
+    
+    return command;
 }
+
+        // if (command.arguments[command.length] == "<") {
+        //     FILE* infile = fopen(command.input_file, "r");
+        //     dup2(fileno(infile), 0);
+        //     fclose(infile); 
+        // }
+        // if (command.arguments[command.length] == ">") {
+        //     FILE* outfile = fopen(command.output_file, "w");
+        //     dup2(fileno(outfile), 1);
+        //     fclose(outfile); 
+        // }
 
 // Executes a shell command
 void ExecuteCommand(struct ShellCommand command) {
     // fork, exec, error handling (invalaid commands, errors, etc)
+    if (strcmp(command.binary, "cd") == 0) {
+        if (chdir(command.arguments[1]) == -1) {
+            printf("Error %i (%s)\n", errno, strerror(errno));
+        } else {
+            
+        }
+    } else {
+        if (fork() == 0) {
+            execvp(command.binary, command.arguments);
+            printf("Error %i (%s)\n", errno, strerror(errno));
+            exit(1);
+        } else wait(NULL);
+    }
+    // check errno
 }
-
-/* redirect stdin from a file snippet
-FILE* infile = fopen(my_input_file, "r");
-dup2(fileno(infile), 0);
-fclose(infile); 
-*/
-
-/* redirect stdout to a file snippet
-FILE* outfile = fopen(my_output_file, "w");
-dup2(fileno(outfile), 1);
-fclose(outfile); 
-*/
 
 //* MAIN
 int main() {
@@ -43,7 +108,7 @@ int main() {
     for(;;) {
         input = CommandPrompt();
         command = ParseCommandLine(input);
-        ExecuteCommand(command);
+        // ExecuteCommand(command);
     }
     exit(0);
 }
